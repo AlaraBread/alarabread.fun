dd9f2c37d0567bb42407d670465e4a8eb31befb7
julia <59781761+AlaraBread@users.noreply.github.com>
2026-03-14 23:24:04 -0400

    simpler version

--- jssat/puzzles.js
+++ jssat/puzzles.js
@@ -1,5 +1,11 @@
 export const puzzles = [
 	{
+		title: "these are different?",
+		key: "these-are-different",
+		expression: `!Number.isNaN(a) && isNaN(a)`,
+		inputs: ["a"],
+	},
+	{
 		title: "hipster null values",
 		key: "hipster-null-values",
 		expression: `!Number.isNaN(a)
