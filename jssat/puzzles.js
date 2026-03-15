5adfef767cd1e6971ca448a6d313f3744d16c305
julia <59781761+AlaraBread@users.noreply.github.com>
2026-03-14 21:23:23 -0400

    polish

--- jssat/puzzles.js
+++ jssat/puzzles.js
@@ -1,8 +1,17 @@
 export const puzzles = [
 	{
 		title: "hipster null values",
-		expression: `Number.isNaN(Number(a))
-	&& !Number.isNaN(a)
+		expression: `!Number.isNaN(a)
+	&& isNaN(a)
+	&& typeof a != "string"
+	&& typeof a != "undefined"
+	&& !a.toString().includes("[")`,
+		inputs: ["a"],
+	},
+	{
+		title: "hipster null values",
+		expression: `!Number.isNaN(a)
+	&& isNaN(a)
 	&& typeof a != "string"
 	&& typeof a != "undefined"
 	&& !a.toString().includes("[")`,
