37731f56e5a9a6d2084e18aa84c44efa01a95bda
julia <59781761+AlaraBread@users.noreply.github.com>
2026-03-14 21:49:41 -0400

    catch exceptions

--- jssat/puzzles.js
+++ jssat/puzzles.js
@@ -1,6 +1,7 @@
 export const puzzles = [
 	{
 		title: "hipster null values",
+		key: "hipster-null-values",
 		expression: `!Number.isNaN(a)
 	&& isNaN(a)
 	&& typeof a != "string"
@@ -9,11 +10,9 @@ export const puzzles = [
 		inputs: ["a"],
 	},
 	{
-		title: "hipster null values",
-		expression: `!Number.isNaN(a)
-	&& typeof a != "string"
-	&& typeof a != "undefined"
-	&& !a.toString().includes("[")`,
-		inputs: ["a"],
+		title: "test puzzle",
+		key: "test-puzzle",
+		expression: `a === b.toString()`,
+		inputs: ["a", "b"],
 	},
 ];
